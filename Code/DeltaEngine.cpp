c6e909460ed2b6a4ece09cea15fb8c4f45eb948a
EnderRayquaza <EnderRayqaza@gmail.com>
2021-10-02 15:03:06 +0200

    Initialisation

---
+++ Code/DeltaEngine.cpp
@@ -0,0 +1,345 @@
+//DeltaEngine.cpp
+#include "DeltaEngine.hpp"
+
+namespace DeltaEngine //Func
+{
+	void print(std::string str, std::string end)
+	{
+		if (end == "none")
+			std::cout << str << std::endl;
+		else
+			std::cout << str << end;
+	}
+
+	json returnJson(std::string jsonPath)
+	{
+		std::ifstream file(jsonPath);
+		if (!file)
+			std::cout << "Erreur : File not open (" << jsonPath << ")" << std::endl;
+		json j{};
+		file >> j;
+		return j;
+	}
+}
+
+namespace DeltaEngine //Project
+{
+	Project::Project(std::string name, int version_Major, int version_minor, bool debug, std::string icon) :
+		m_name(name), m_ver_M(version_Major), m_ver_m(version_minor), m_debug(debug), m_ico(icon)
+	{}
+
+	std::string Project::get_title()
+	{
+		std::string title{ m_name };
+		if (m_debug)
+			title += " v." + std::to_string(m_ver_M) + "." + std::to_string(m_ver_m);
+		return title;
+	}
+
+}
+
+namespace DeltaEngine //Game
+{
+	Game::Game(Project& prj, b2Vec2& gravity, float timeStep, int32 velIt, int32 posIt) :
+		m_prj(prj), m_win(sf::VideoMode(800, 600), prj.get_title(), sf::Style::Default),
+		m_gravity(gravity), m_world(m_gravity), m_timeStep(timeStep), m_velIt(velIt), m_posIt(posIt)
+	{}
+
+	sf::RenderWindow& Game::get_win()
+	{
+		return m_win;
+	}
+
+	b2World& Game::get_world()
+	{
+		return m_world;
+	}
+
+	float Game::get_timeStep()
+	{
+		return m_timeStep;
+	}
+
+	int32 Game::get_velIt()
+	{
+		return m_velIt;
+	}
+
+	int32 Game::get_posIt()
+	{
+		return m_posIt;
+	}
+
+	void Game::draw()
+	{
+		std::vector<Part> vPartObj, vPartEnt;
+		sf::Sprite sprite;
+		for (unsigned int i{ 0 }; i < m_vObj.size(); i++)
+		{	
+			for (int j{ 0 }; j < m_vObj[i].get_nb_part(); j++)
+			{
+				vPartObj.push_back(m_vObj[i].get_Vpart()[j]);
+			}
+		}
+		for (unsigned int i{ 0 }; i < m_vEnt.size(); i++)
+		{
+			for (int j{ 0 }; j < m_vEnt[i].get_nb_part(); j++)
+			{
+				vPartEnt.push_back(m_vEnt[i].get_Vpart()[j]);
+			}
+		}
+
+		for (unsigned int i{ 0 }; i < vPartObj.size(); i++)
+		{
+			if (!vPartObj[i].get_textureOn())
+			{
+				for (unsigned int j{ 0 }; j < vPartObj[i].get_nb_vtx(); j++)
+				{
+					vPartObj[i].get_shape().setPosition(vPartObj[i].get_pos());
+					vPartObj[i].get_shape().setRotation(vPartObj[i].get_angle());
+					vPartObj[i].get_shape().setOutlineColor(sf::Color::Blue);
+					vPartObj[i].get_shape().setFillColor(sf::Color::Transparent);
+					vPartObj[i].get_shape().setOutlineThickness(2.f);
+				}
+			}
+			m_win.draw(vPartObj[i].get_shape());
+		}
+		for (unsigned int i{ 0 }; i < vPartEnt.size(); i++)
+		{
+			for (unsigned int j{ 0 }; j < vPartEnt[i].get_nb_vtx(); j++)
+			{
+				vPartEnt[i].get_shape().setPosition(vPartEnt[i].get_pos());
+				vPartEnt[i].get_shape().setRotation(vPartEnt[i].get_angle());
+				vPartEnt[i].get_shape().setOutlineColor(sf::Color::Red);
+				vPartEnt[i].get_shape().setFillColor(sf::Color::Transparent);
+				vPartEnt[i].get_shape().setOutlineThickness(2.f);
+			}
+			m_win.draw(vPartEnt[i].get_shape());
+		}
+	}
+
+	void Game::addObj(Object& obj)
+	{
+		m_vObj.push_back(obj);
+	}
+
+	void Game::addEnt(Entity& ent)
+	{
+		m_vEnt.push_back(ent);
+	}
+
+}
+
+namespace DeltaEngine
+{
+	Part::Part(std::string jsonPath, b2World& world, sf::Vector2f pos)
+	{
+		//---JSON---
+		json j{ returnJson(jsonPath) }; //Collect data from json.
+		//---Class---
+		m_coef = j["coef"];
+		m_priority = j["priority"];
+		m_type = j["typeBody"]; //Define the type of body (Static/Kinematic/Dynamic).
+		m_nb_vtx = j["nb_vtx"]; //Define the nb of vertices.
+		//---SFML---
+		m_shape = sf::ConvexShape(m_nb_vtx); //Make a shape with [nb_vtx] vertices.
+		double vtxPosX{ 0 }, vtxPosY{ 0 };
+		for (int i{ 0 }; i < m_nb_vtx; i++)
+		{
+			vtxPosX = j["vtxPos"][i][0]; vtxPosY = j["vtxPos"][i][1];
+			m_shape.setPoint(i, sf::Vector2f(vtxPosX*m_coef, vtxPosY*m_coef));
+		}
+		m_tex.loadFromFile(j["texPath"]);
+		m_shape.setTexture(&m_tex);
+		//Set the vertices to their own pos.
+		//---Box2D---
+		b2Vec2 vertices[b2_maxPolygonVertices]; //Create an array of vertices.
+		for (int i{ 0 }; i < m_nb_vtx; i++)
+		{
+			vertices[i].Set(j["vtxPos"][i][0], j["vtxPos"][i][1]);
+		}
+		//Set the vertices to their own pos.
+		b2PolygonShape partShape; //Create a shape for the part.
+		partShape.Set(vertices, m_nb_vtx); //Set the vertices for the shape.
+		b2FixtureDef fixtureDef; //Create a FixtureDef.
+		fixtureDef.shape = &partShape; //Set the shape to the FixtureDef.
+		fixtureDef.density = j["density"]; //Set the density of the part.
+		fixtureDef.friction = j["friction"]; //Set the friction of the part.
+		fixtureDef.restitution = j["restitution"]; //...
+		b2BodyDef bodyDef; //Create a BodyDef.
+		float posX{ j["pos"][0] + pos.x }, posY{ j["pos"][1] + pos.y };
+		bodyDef.position.Set(posX, posY);
+		//print(std::to_string(posX) + "/" + std::to_string(posY));
+		switch (m_type)
+		{
+		case STATICBODY:
+			bodyDef.type = b2_staticBody;
+			break;
+		case KINEMATICBODY:
+			bodyDef.type = b2_kinematicBody;
+			break;
+		case DYNAMICBODY:
+			bodyDef.type = b2_dynamicBody;
+			bodyDef.fixedRotation = true;
+			break;
+		default:
+			break;
+		}
+		//Set the type of the part.
+		m_body = world.CreateBody(&bodyDef); //Create the part.
+		m_body->CreateFixture(&fixtureDef); //Set the fixture to the part.
+	}
+
+	double Part::get_coef()
+	{
+		return m_coef;
+	}
+
+	int Part::get_priority()
+	{
+		return m_priority;
+	}
+
+	int Part::get_nb_vtx()
+	{
+		return m_nb_vtx;
+	}
+
+	sf::ConvexShape& Part::get_shape()
+	{
+		return m_shape;
+	}
+
+	b2Body* Part::get_body()
+	{
+		return m_body;
+	}
+
+	sf::Vector2f Part::get_pos(bool inPx)
+	{
+		b2Vec2 pos{ m_body->GetPosition() };
+		if(inPx)
+			return sf::Vector2f(pos.x*m_coef, pos.y*m_coef);
+		else
+			return sf::Vector2f(pos.x, pos.y);
+	}
+
+	double Part::get_angle(bool inDeg)
+	{
+		if (inDeg)
+			return m_body->GetAngle() * RAD2DEG;
+		else
+			return m_body->GetAngle();
+	}
+}
+
+namespace DeltaEngine //Object
+{
+	Object::Object(std::string jsonPath, b2World& world, sf::Vector2f pos)
+	{
+		json j{ returnJson(jsonPath) }; //Collect data from json.
+		m_id = j["id"]; //Set the id.
+		m_name = j["name"]; //...
+		m_nb_part = j["nb_part"]; //...
+		for (int i{ 0 }; i < m_nb_part; i++)
+		{
+			m_Vpart.push_back(Part(j["parts"][i], world, pos));
+		}
+		//Create and add part to the object.
+	}
+
+	int Object::get_nb_part()
+	{
+		return m_nb_part;
+	}
+
+	std::vector<Part> Object::get_Vpart()
+	{
+		return m_Vpart;
+	}
+
+}
+
+namespace DeltaEngine //Entity
+{
+	Entity::Entity(std::string jsonPath, b2World& world, sf::Vector2f pos):Object::Object(jsonPath, world, pos)
+	{
+		json j{returnJson(jsonPath)};
+		m_hpMax = j["hp"];
+		m_hp = j["hp"];
+		m_isAlive = true;
+	}
+
+	bool Entity::verifyIfAlive()
+	{
+		if (m_hp <= 0)
+		{
+			m_hp = 0;
+			print(m_name + "is dead...");
+			m_isAlive = false;
+		}
+		else
+		{
+			m_isAlive = true;
+		}
+		return m_isAlive;
+	}
+
+	void Entity::damage(double val)
+	{
+		m_hp -= val;
+		verifyIfAlive();
+	}
+
+	void Entity::heal(double val)
+	{
+		m_hp += val;
+		if (m_hp > m_hpMax)
+			m_hp = m_hpMax;
+	}
+
+	void Entity::move(int dir, double val, double drag, double acc)
+	{
+		b2Vec2 vel{ 0.f, 0.f };
+		for (int i{ 0 }; i < m_nb_part; i++)
+		{
+			vel += m_Vpart[i].get_body()->GetLinearVelocity();
+		}
+		double desiredVel = 0;
+		switch (dir)
+		{
+		case -1:  desiredVel = b2Min(vel.x - acc, -val); break;
+		case 0:  desiredVel = vel.x * drag; break;
+		case 1: desiredVel = b2Max(vel.x + acc, val); break;
+		}
+		double velChange{ desiredVel - vel.x };
+		double force{ 0 };
+		for (int i{ 0 }; i < m_nb_part; i++)
+		{
+			force = m_Vpart[i].get_body()->GetMass() * velChange / (1 / 60.0); //f = mv/t
+			m_Vpart[i].get_body()->ApplyForce(b2Vec2(force, 0), m_Vpart[i].get_body()->GetWorldCenter(), true);
+		}
+	}
+
+	void Entity::jump(double val)
+	{
+		float force{ 0 };
+		for (int k{ 0 }; k < val; k++)
+		{
+			for (int i{ 0 }; i < m_nb_part; i++)
+			{
+				force = m_Vpart[i].get_body()->GetMass() * 10 / (1 / 60.0); //f = mv/t
+				force /= 6.0;
+				m_Vpart[i].get_body()->ApplyForce(b2Vec2(0, force), m_Vpart[i].get_body()->GetWorldCenter(), true);
+			}
+		}
+	}
+
+	void Entity::tp(b2Vec2 pos)
+	{
+		for (int i{ 0 }; i < m_nb_part; i++)
+		{
+			m_Vpart[i].get_body();
+		}
+	}
+}
---
+++ Code/DeltaEngine.hpp
@@ -0,0 +1,128 @@
+//DeltaEngine.hpp
+#pragma once
+
+#include <fstream>
+#include <iostream>
+#include <string>
+#include <vector>
+
+#include <SFML/Window.hpp>
+#include <SFML/Graphics.hpp>
+
+#include <TGUI/TGUI.hpp>
+
+#include <box2d/box2d.h>
+
+#include <nlohmann/json.hpp>
+
+#define RAD2DEG 57.29577951
+#define DEG2RAD 0.01745329252
+
+#define STATICBODY 0
+#define KINEMATICBODY 1
+#define DYNAMICBODY 2
+
+using json = nlohmann::json;
+
+namespace DeltaEngine
+{
+    class Project;
+	class Game;
+	class Object;
+	class Entity;
+
+
+	void print(std::string str, std::string end="none");
+	json returnJson(std::string jsonPath);
+
+	class Project
+	{
+	public:
+		Project(std::string name, int version_Major, int version_minor, bool debug, std::string icon);
+		std::string get_title();
+	protected:
+		std::string m_name;
+		int m_ver_M;
+		int m_ver_m;
+		bool m_debug;
+		std::string m_ico;
+	};
+
+	class Game
+	{
+	public:
+		Game(Project& prj, b2Vec2& gravity, float timeStep = 1.f/60.f, int32 velIt = 6, int32 posIt = 3);
+		sf::RenderWindow& get_win();
+		b2World& get_world();
+		float get_timeStep();
+		int32 get_velIt();
+		int32 get_posIt();
+
+		void draw();
+		void addObj(Object& obj);
+		void addEnt(Entity& ent);
+
+	protected:
+		Project m_prj;
+		sf::RenderWindow m_win;
+		std::vector<Object> m_vObj;
+		std::vector<Entity> m_vEnt;
+		b2Vec2 m_gravity;
+		b2World m_world;
+		float m_timeStep;
+		int32 m_velIt;
+		int32 m_posIt;
+	};
+
+	class Part
+	{
+	public:
+		Part(std::string jsonPath, b2World& world, sf::Vector2f pos = sf::Vector2f(0, 0));
+		double get_coef();
+		int get_priority();
+		int get_nb_vtx();
+		sf::ConvexShape& get_shape();
+		sf::Texture& get_texture();
+		bool get_textureOn();
+		b2Body* get_body();
+		sf::Vector2f get_pos(bool inPx=true);
+		double get_angle(bool inDeg = true);
+	protected:
+		double m_coef;
+		int m_priority;
+		int m_type;
+		int m_nb_vtx;
+		sf::ConvexShape m_shape;
+		sf::Texture m_tex;
+		b2Body* m_body;
+	};
+
+	class Object
+	{
+	public:
+		Object(std::string jsonPath, b2World& world, sf::Vector2f pos = sf::Vector2f(0, 0));
+		int get_nb_part();
+		std::vector<Part> get_Vpart();
+	protected:
+		int m_id;
+		std::string m_name;
+		int m_nb_part;
+		std::vector<Part> m_Vpart;
+	};
+
+	class Entity : public Object
+	{
+	public:
+		Entity(std::string jsonPath, b2World& world, sf::Vector2f pos = sf::Vector2f(0, 0));
+		bool verifyIfAlive();
+		void damage(double val);
+		void heal(double val);
+		void move(int dir, double val, double drag = 0.98, double acc = 0.1);
+		void jump(double val);
+		void tp(b2Vec2 pos);
+	protected:
+		double m_hpMax;
+		double m_hp;
+		bool m_isAlive;
+	};
+}
