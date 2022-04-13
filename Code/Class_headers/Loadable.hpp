46d8e5bcf6ea86d20bda471d55110be6ecbb9b64
EnderRayquaza <78414179+EnderRayquaza@users.noreply.github.com>
2022-04-13 21:59:30 +0200

    Create Loadable.hpp

---
+++ Code/Class_headers/Loadable.hpp
@@ -0,0 +1,19 @@
+#pragma once
+
+#include "../config.hpp"
+
+namespace DeltaEngine
+{
+	class Loadable
+	{
+	public:
+		Loadable() = default;
+		Loadable(jsonStr path) :_path{ path }
+		{}
+		~Loadable() = default;
+
+		virtual bool load() = 0;
+
+		jsonStr _path;
+	};
+}
